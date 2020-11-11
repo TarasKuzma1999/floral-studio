e70486965a773927221946bbb69b3f99f5e4a56e
TarasKuzma1999 <56336985+TarasKuzma1999@users.noreply.github.com>
2020-11-11 17:49:29 +0200

    added servises

--- js/index.js
+++ js/index.js
@@ -1,3 +1,52 @@
 $('.burger-meny-icon').click(function (event) {
     $('.burger-meny').toggleClass('active');
 })
+
+$('.decoration').mouseover(function (ev) {
+    if ($(window).width() >= 990) {
+        $('.decoration p').css({
+            'opacity': '1'
+        })
+    }
+})
+
+$('.decoration').mouseout(function (ev) {
+    if ($(window).width() >= 990) {
+        $('.decoration p').css({
+            'opacity': '0'
+        })
+    }
+})
+
+$('.floral-design').mouseover(function (ev) {
+    if ($(window).width() >= 990) {
+        $('.floral-design p').css({
+            'opacity': '1'
+        })
+    }
+})
+
+$('.floral-design').mouseout(function (ev) {
+    if ($(window).width() >= 990) {
+        $('.floral-design p').css({
+            'opacity': '0'
+        })
+    }
+})
+
+$('.events').mouseover(function (ev) {
+    if ($(window).width() >= 990) {
+        $('.events p').css({
+            'opacity': '1'
+        })
+    }
+})
+
+$('.events').mouseout(function (ev) {
+    if ($(window).width() >= 990) {
+        $('.events p').css({
+            'opacity': '0'
+        })
+    }
+})
+
